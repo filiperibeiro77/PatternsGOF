1d03972614d528f13b998acd938ce468b839bb2b
Filipe Ribeiro <filiperibeirom7@gmail.com>
2016-06-17 00:47:54 -0300

    improving state project

--- State/src/app/Mario.java
+++ State/src/app/Mario.java
@@ -1,26 +1,25 @@
 package app;
 
 public class Mario {
-	MarioState estado;
-	
-	public Mario (){
-		new MarioPequeno();
-	}
-
-	public void pegarCogumelo() {
-		estado = estado.pegarCogumelo();
-	}
-	
-	public void pegarFlor() {
-		estado = estado.pegarFlor();
-	}
-	
-	public void pegarPena() {
-		estado = estado.pegarPena();
-	}
-	
-	public void levarDano() {
-		estado = estado.levarDano();
-	}
-	
+	 protected MarioState estado;
+     
+	    public Mario() {
+	        estado = new MarioPequeno();
+	    }
+	 
+	    public void pegarCogumelo() {
+	        estado = estado.pegarCogumelo();
+	    }
+	 
+	    public void pegarFlor() {
+	        estado = estado.pegarFlor();
+	    }
+	 
+	    public void pegarPena() {
+	        estado = estado.pegarPena();
+	    }
+	 
+	    public void levarDano() {
+	        estado = estado.levarDano();
+	    }	
 }
